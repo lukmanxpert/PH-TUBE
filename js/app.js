e968719bc9b618c8dc1e72a27facd7a1f60aea72
lukmanxpert <lukmanmiah2004@gmail.com>
2024-10-04 15:50:25 +0600

    added videos desc sections

--- js/app.js
+++ js/app.js
@@ -25,18 +25,21 @@ const displayVideos = (videos) => {
   const videosSection = document.getElementById("videos-section");
   videos.forEach((video) => {
     const videoDiv = document.createElement("div");
-    videoDiv.classList = "card card-compact";
+    videoDiv.classList = "card-compact";
     videoDiv.innerHTML = `
-    <figure>
-    <img
+    <figure class="h-[200px]">
+    <img class="h-full w-full object-cover"
       src=${video.thumbnail}
       alt="Shoes" />
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+  <div class="pt-2 mb-4 flex gap-2">
+    <div>
+        <img class="h-8 w-8 object-cover rounded-full" src=${video.authors[0].profile_picture}>
+    </div>
+    <div>
+    <h3 class="font-bold text-lg">${video.title}</h3>
+    <p>${video.authors[0].profile_name}</p>
+    <p>${video.others.views} views</p>
     </div>
   </div>
     `;
@@ -45,3 +48,22 @@ const displayVideos = (videos) => {
 };
 loadVideos();
 loadCatagorys();
+
+// {
+//     "category_id": "1001",
+//     "video_id": "aaaa",
+//     "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+//     "title": "Shape of You",
+//     "authors": [
+//         {
+//             "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+//             "profile_name": "Olivia Mitchell",
+//             "verified": ""
+//         }
+//     ],
+//     "others": {
+//         "views": "100K",
+//         "posted_date": "16278"
+//     },
+//     "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+// }
